9b888bdc78aed5480ba84fbca812bf07b56335a0
Isaac <wucisaac@outlook.com>
2023-02-27 14:19:09 -0300

    formateado autopep8

--- funciones/calculadorCubo.py
+++ funciones/calculadorCubo.py
@@ -1,81 +1,91 @@
 def calcularPiezas(lado_a, lado_b, lado_c):
-   cubo = str(lado_a) + "x" + str(lado_b) + "x" + str(lado_c)
-   if lado_a==1 and lado_b==1 and lado_c==1: #1x1x1
-        return {"cubo":cubo,
-               "piezas":1, 
-               "front_plus_back":0, 
-               "aristas_centrales":0, 
-               "centro_left_plus_right":0,
-               "centro_up_plus_down":0}
-   if tiene_dos_profundidad(lado_a, lado_b, lado_c):
-       return dos_dimensiones_resultado(lado_a, lado_b, lado_c, cubo)
-   
-   cara = cara_cuadrado(lado_a, lado_b)
-   sup_inf = sup_inf_cuadrado(cara)
-   aristas_centrales = aristas_centrales_cuadrado(cara,lado_a, lado_b)
-   centro = (centro_cuadrado(cara,lado_a, lado_b))
-   centro_rect = 0
+    cubo = str(lado_a) + "x" + str(lado_b) + "x" + str(lado_c)
+    if lado_a == 1 and lado_b == 1 and lado_c == 1:  # 1x1x1
+        return {"cubo": cubo,
+                "piezas": 1,
+                "front_plus_back": 0,
+                "aristas_centrales": 0,
+                "centro_left_plus_right": 0,
+                "centro_up_plus_down": 0}
+    if tiene_dos_profundidad(lado_a, lado_b, lado_c):
+        return dos_dimensiones_resultado(lado_a, lado_b, lado_c, cubo)
 
-   if is_rectangular(lado_a, lado_b, lado_c): #si es rectangular se calcula los dos centros que son diferentes de las otras caras
-     centro *= 2
-     centro_rect = (centro_rectangular(lado_b, lado_c))*2
+    cara = cara_cuadrado(lado_a, lado_b)
+    sup_inf = sup_inf_cuadrado(cara)
+    aristas_centrales = aristas_centrales_cuadrado(cara, lado_a, lado_b)
+    centro = (centro_cuadrado(cara, lado_a, lado_b))
+    centro_rect = 0
 
-   else:
-       centro *= 4
-       
-   total = sup_inf + aristas_centrales+(centro + centro_rect)
-   return {"cubo":cubo,
-           "piezas":total, 
-           "front_plus_back":sup_inf, 
-           "aristas_centrales":aristas_centrales, 
-           "centro_left_plus_right":centro,
-           "centro_up_plus_down":centro_rect}
+    # si es rectangular se calcula los dos centros que son diferentes de las otras caras
+    if is_rectangular(lado_a, lado_b, lado_c):
+        centro *= 2
+        centro_rect = (centro_rectangular(lado_b, lado_c))*2
+
+    else:
+        centro *= 4
+
+    total = sup_inf + aristas_centrales+(centro + centro_rect)
+    return {"cubo": cubo,
+            "piezas": total,
+            "front_plus_back": sup_inf,
+            "aristas_centrales": aristas_centrales,
+            "centro_left_plus_right": centro,
+            "centro_up_plus_down": centro_rect}
 
 # si tiene dos profundidad
+
+
 def tiene_dos_profundidad(lado_a, lado_b, lado_c):
-    if lado_a==2 or lado_b==2 or lado_c==2:
+    if lado_a == 2 or lado_b == 2 or lado_c == 2:
         return True
     return False
+
+
 def dos_dimensiones_resultado(lado_a, lado_b, lado_c, cubo):
     cara = 0
     sup_inf = 0
-    if lado_a==2:
+    if lado_a == 2:
         cara = lado_b*lado_c
         sup_inf = cara*2
-    elif lado_b==2:
+    elif lado_b == 2:
         cara = lado_a*lado_c
         sup_inf = cara*2
-    elif lado_c==2:
+    elif lado_c == 2:
         cara = lado_a*lado_b
         sup_inf = cara*2
-    return {"cubo":cubo,
-            "piezas":sup_inf,
-            "front_plus_back":sup_inf,
-            "aristas_centrales":0, 
-            "centro_left_plus_right":0,
-            "centro_up_plus_down":0}
+    return {"cubo": cubo,
+            "piezas": sup_inf,
+            "front_plus_back": sup_inf,
+            "aristas_centrales": 0,
+            "centro_left_plus_right": 0,
+            "centro_up_plus_down": 0}
 
 
-    
 # calcular por partes el cubo solo si es de partes cuadradas
 def cara_cuadrado(alto, ancho):
     return alto*ancho
+
+
 def sup_inf_cuadrado(cara):
     return cara*2
+
+
 def aristas_centrales_cuadrado(cara, alto, ancho):
     return ((cara - (alto*2)) - centro_cuadrado(cara, alto, ancho))*2
+
+
 def centro_cuadrado(cara, alto, ancho):
     return (cara-(alto*2)) - ((ancho-2)*2)
 
 # calculo para cubo rectangular
+
+
 def is_rectangular(lado_a, lado_b, lado_c):
-    if lado_a!=lado_b or lado_a!=lado_c or lado_b!=lado_a or lado_b!=lado_c or lado_c!=lado_a or lado_c!=lado_b:
+    if lado_a != lado_b or lado_a != lado_c or lado_b != lado_a or lado_b != lado_c or lado_c != lado_a or lado_c != lado_b:
         return True
     return False
 
+
 def centro_rectangular(ancho, profundidad):
-    cara= ancho * profundidad
+    cara = ancho * profundidad
     return (cara - (ancho*2)) - ((profundidad-2)*2)
-
-
-
--- funciones/test/test_calculadorCubo.py
+++ funciones/test/test_calculadorCubo.py
@@ -1,19 +1,25 @@
 from funciones.calculadorCubo import calcularPiezas
 import pytest
 
+
 @pytest.mark.parametrize(
     "alto, ancho, profundidad, esperado",
     [
-    #tests cubos cuadrados
-   (10, 10, 10, {'cubo': '10x10x10', 'piezas': 488, 'front_plus_back': 200, 'aristas_centrales': 32, 'centro_left_plus_right': 256, 'centro_up_plus_down':0}) ,
-   (4, 4, 4, {'cubo': '4x4x4', 'piezas': 56, 'front_plus_back': 32, 'aristas_centrales': 8, 'centro_left_plus_right': 16, 'centro_up_plus_down':0}) ,
-   (3, 3, 3, {'cubo': '3x3x3', 'piezas': 26, 'front_plus_back': 18, 'aristas_centrales': 4, 'centro_left_plus_right': 4, 'centro_up_plus_down':0}) ,
-   (2, 2, 2, {'cubo': '2x2x2', 'piezas': 8, 'front_plus_back': 8, 'aristas_centrales': 0, 'centro_left_plus_right': 0, 'centro_up_plus_down':0}) ,
-   (1, 1, 1, {'cubo': '1x1x1', 'piezas': 1, 'front_plus_back': 0, 'aristas_centrales': 0, 'centro_left_plus_right': 0, 'centro_up_plus_down':0}) ,
-    #tests cubos rectangulares
-   (7, 3, 3, {'cubo': '7x3x3', 'piezas': 58, 'front_plus_back': 42, 'aristas_centrales': 4, 'centro_left_plus_right': 10, 'centro_up_plus_down':2}) ,
+        # tests cubos cuadrados
+        (10, 10, 10, {'cubo': '10x10x10', 'piezas': 488, 'front_plus_back': 200,
+         'aristas_centrales': 32, 'centro_left_plus_right': 256, 'centro_up_plus_down': 0}),
+        (4, 4, 4, {'cubo': '4x4x4', 'piezas': 56, 'front_plus_back': 32,
+         'aristas_centrales': 8, 'centro_left_plus_right': 16, 'centro_up_plus_down': 0}),
+        (3, 3, 3, {'cubo': '3x3x3', 'piezas': 26, 'front_plus_back': 18,
+         'aristas_centrales': 4, 'centro_left_plus_right': 4, 'centro_up_plus_down': 0}),
+        (2, 2, 2, {'cubo': '2x2x2', 'piezas': 8, 'front_plus_back': 8,
+         'aristas_centrales': 0, 'centro_left_plus_right': 0, 'centro_up_plus_down': 0}),
+        (1, 1, 1, {'cubo': '1x1x1', 'piezas': 1, 'front_plus_back': 0,
+         'aristas_centrales': 0, 'centro_left_plus_right': 0, 'centro_up_plus_down': 0}),
+        # tests cubos rectangulares
+        (7, 3, 3, {'cubo': '7x3x3', 'piezas': 58, 'front_plus_back': 42,
+         'aristas_centrales': 4, 'centro_left_plus_right': 10, 'centro_up_plus_down': 2}),
     ]
 )
 def test_calculadorCubo(alto, ancho, profundidad, esperado):
     assert calcularPiezas(alto, ancho, profundidad) == esperado
-    
