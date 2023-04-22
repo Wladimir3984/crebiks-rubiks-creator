bd1e041e53a4e5c39ca7a3943b7563ec2f9e8fbe
Wladimir <83993271+Wladimir3984@users.noreply.github.com>
2023-04-21 23:31:48 -0400

    Update settings.py

--- Crebiks/settings.py
+++ Crebiks/settings.py
@@ -20,10 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-if DEBUG:
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = config('SECRET_KEY', default='324324itIsOnlyForDevelopmentItsBetterDontUseTheDefaultaskd873264732') # default value is for development only
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -123,3 +119,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = config('SECRET_KEY', default='324324itIsOnlyForDevelopmentItsBetterDontUseTheDefaultaskd873264732') # default value is for development only
+
