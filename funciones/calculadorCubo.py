7ded9d96d30f12e0f359059077b5252ff980c141
Wladimir <83993271+Wladimir3984@users.noreply.github.com>
2023-04-23 17:34:27 -0400

    Update calculadorCubo.py

--- funciones/calculadorCubo.py
+++ funciones/calculadorCubo.py
@@ -1,12 +1,11 @@
 def get_cube_details(lado_a, lado_b, lado_c):
     try:
-        if not (int == type(lado_a) == type(lado_b) == type(lado_c)):
+        if not (int== type(lado_a) == type(lado_b) == type(lado_c)):
             return False  # no es de tipo entero
         if lado_a <= 0 or lado_b <= 0 or lado_c <= 0:
             return False  # no se aceptan numeros negativos o cero
 
-        # ordenar lados de mayor a menor
-        lados = [lado_a, lado_b, lado_c]
+        lados = [lado_a, lado_b, lado_c]  # ordenar lados de mayor a menor
         lados.sort(reverse=True)
         return calcularPiezas(lados[0], lados[1], lados[2])
     except:  # testing si recive un string
